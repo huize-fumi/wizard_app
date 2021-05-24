3cee31c198e5641793c97af48dbf4162baa49812
huize-fumi <arin0ma2@yahoo.co.jp>
2021-05-24 14:42:50 +0900

    userモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, :age ,presence: true
+  has_one :address
          
 end
